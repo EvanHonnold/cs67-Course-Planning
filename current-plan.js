27f481c545bcef4fefeb30ecb1a83bebcdd71e1e
Evan Honnold <ehonnold@gmail.com>
2017-05-09 22:56:11 -0400

    Finally fixed main
    Played around with the main screen and finally figured out how to
    prevent additions to the panes from causing them to no longer be the
    same size compared to each other.

--- current-plan.js
+++ current-plan.js
@@ -1,6 +1,15 @@
+/**
+ * Written by Evan Honnold
+ * 
+ * Currently incomplete
+ */
+
 function generateCurrentPlan(element, document){
 
-	
+	var section_title = document.createElement("h1");
+	var title_text = document.createTextNode("Current Plan");
+	section_title.appendChild(title_text);
+	//element.appendChild(section_title);
 
 	console.log("current plan loaded");
 }

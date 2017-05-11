de903b810dbe287d7d535491b580d4495f4e400c
Evan Honnold <ehonnold@gmail.com>
2017-05-11 12:59:09 -0400

    minor change to add title
    removed comments from current-plan.js and cleaned up main-screen.html

--- current-plan.js
+++ current-plan.js
@@ -9,7 +9,7 @@ function generateCurrentPlan(element, document){
 	var section_title = document.createElement("h1");
 	var title_text = document.createTextNode("Current Plan");
 	section_title.appendChild(title_text);
-	//element.appendChild(section_title);
+	element.appendChild(section_title);
 
 	console.log("current plan loaded");
 }

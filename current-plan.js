f09a3bb28a4fc1dfd5d0885fe721a43b13ee2ea6
Evan Honnold <ehonnold@gmail.com>
2017-05-11 23:33:30 -0400

    cleaned up main screen
    learned more css and realized the screen could be simplified a lot ...
    also added dividers between the three sections to make them seem more
    like the Dartmouth website

--- current-plan.js
+++ current-plan.js
@@ -16,8 +16,8 @@ function generateCurrentPlan(element){
 	// for testing, until this script can accept term 
 	// objects to define what terms will be shown on the plan:
 
-	//var termDisplay = getTermDisplay();
-	//element.appendChild(termDisplay);
+	var termDisplay = getTermDisplay();
+	element.appendChild(termDisplay);
 
 
 	console.log("current plan loaded");
@@ -26,5 +26,45 @@ function generateCurrentPlan(element){
 // for testing, until this script can accept term 
 // objects to define what terms will be shown on the plan:
 function getTermDisplay(){
+	var container = document.createElement("div");
+	container.classList.add('term-listing-container');
+	var nameDisplay = document.createElement("span");
+	var nameNode = document.createTextNode("Fall 2017");
+	nameDisplay.appendChild(nameNode);
+	nameDisplay.classList.add("term-listing-term-name");
+	container.appendChild(nameDisplay);
 
+	var coursesDisplay = document.createElement("div");
+	coursesDisplay.classList.add('term-listing-courses-container');
+	container.appendChild(coursesDisplay);
+
+	var course1 = document.createElement("div");
+	var course2 = document.createElement("div");
+	var course3 = document.createElement("div");
+	course1.classList.add("course-box");
+	course2.classList.add("course-box");
+	course3.classList.add("course-box");
+	var text1 = document.createTextNode("Course Name Here  .........sdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdf");
+	var text2 = document.createTextNode("Course Name Here");
+	var text3 = document.createTextNode("Course Name Here");
+	course1.appendChild(text1);
+	course2.appendChild(text2);
+	course3.appendChild(text3);
+	
+	coursesDisplay.appendChild(course1);
+	coursesDisplay.appendChild(course2);
+	coursesDisplay.appendChild(course3);
+
+
+	// var text_1 = document.createElement("span");
+	// var text_text = document.createTextNode("text");
+	// text_1.appendChild(text_text);
+	// var text_2 = document.createElement("span");
+	// var text_text2 = document.createTextNode("text");
+	// text_2.appendChild(text_text2);
+	
+	// nameDisplay.appendChild(text_1);
+	// coursesDisplay.appendChild(text_2);
+
+	return container;
 }

12767252f528ec5dd74b583b99b006ea6b1eb732
Evan Honnold <ehonnold@gmail.com>
2017-05-19 14:16:41 -0400

    added course data file & loading

--- javascript_objects/course.js
+++ javascript_objects/course.js
@@ -1,3 +1,20 @@
 function loadCourses(){
     console.log("loadCourses called");
+
+    var filePath = "data/courses.txt";
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(req.readyState === XMLHttpRequest.DONE){
+            if(req.status === 200){
+                var allText = req.responseText;
+                console.log(allText);
+            }
+        }
+    }
+    req.open("get", filePath, true);
+    req.send(null);
+}
+
+function Course(department, number, name, information, distribList){
+
 }
